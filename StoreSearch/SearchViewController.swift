08766781377a7a82e5fa62924158fa8b4ab1e6fa
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-07 22:03:20 +0200

    add some fake data to test Search Bar and adjust it's position on top

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -4,16 +4,47 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
 
+    var searchResults = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right: 0)
     }
-
 }
 
 //MARK: Search Bar Delegate
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("The search text is: '\(searchBar.text!)'")
+        searchBar.resignFirstResponder()
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(
+                String(
+                    format: "Fake Result %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell!.textLabel!.text = searchResults[indexPath.row]
+
+        return cell!
     }
 }
 
