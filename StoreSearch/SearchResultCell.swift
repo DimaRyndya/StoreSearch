a48131e4e7cf754f4cbc0ac1d2143e9e47051968
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-08 19:41:34 +0200

    Add cell on tap color and App Icon

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -8,6 +8,10 @@ class SearchResultCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
+        selectedBackgroundView = selectedView
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
