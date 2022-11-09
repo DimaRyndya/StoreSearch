1ad1b9a156d19fbb826012b1d51befffee34e317
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-09 21:35:33 +0200

    Modify SearchResult by adding variables, add CodingKeys and put fetched results in local array

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -7,14 +7,30 @@ class ResultArray: Codable {
 
 class SearchResult: Codable, CustomStringConvertible {
     var description: String {
-      return "\nResult - Name: \(name), Artist Name: \(artistName ?? "None")"
+        return "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
     }
 
 
     var artistName: String? = ""
     var trackName: String? = ""
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var storeURL: String? = ""
+    var genre = ""
 
     var name: String {
         return trackName ?? ""
+    }
+
+    enum CodingKeys: String, CodingKey {
+      case imageSmall = "artworkUrl60"
+      case imageLarge = "artworkUrl100"
+      case storeURL = "trackViewUrl"
+      case genre = "primaryGenreName"
+      case kind, artistName, trackName
+      case trackPrice, currency
     }
 }
