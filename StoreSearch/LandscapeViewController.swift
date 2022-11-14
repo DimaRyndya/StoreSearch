ab3daced1e9a71d02c78e8428bcef58d0c013ab6
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-14 19:44:08 +0200

    Create scrollView and pageControl for the new Landscape scene and add outlets for them. Remove all auto layaut constraints and set custom.

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -1,8 +1,30 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Remove constraints from main view
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        // Remove constraints for page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        // Remove constraints for scroll view
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let safeFrame = view.safeAreaLayoutGuide.layoutFrame
+        scrollView.frame = safeFrame
+        pageControl.frame = CGRect(
+            x: safeFrame.origin.x,
+            y: safeFrame.size.height - pageControl.frame.size.height,
+            width: safeFrame.size.width,
+            height: pageControl.frame.size.height)
     }
 }
