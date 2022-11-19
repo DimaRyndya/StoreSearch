26007b1dfbd849a2694bd72f8ccd26bf2056d105
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-19 20:21:57 +0200

    Change tileButtons method's variables viewWidth and viewHeight to properly fit buttons on iPad, make some changes to improve UI fonts and sizes to respect iPad sizes

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -123,8 +123,8 @@ class LandscapeViewController: UIViewController {
         var rowsPerPage = 0
         var marginX: CGFloat = 0
         var marginY: CGFloat = 0
-        let viewWidth = scrollView.bounds.size.width
-        let viewHeight = scrollView.bounds.size.height
+        let viewWidth = UIScreen.main.bounds.size.width
+        let viewHeight = UIScreen.main.bounds.size.height
         columnsPerPage = Int(viewWidth / itemWidth)
         rowsPerPage = Int(viewHeight / itemHeight)
         marginX = (viewWidth - (CGFloat(columnsPerPage) * itemWidth)) * 0.5
